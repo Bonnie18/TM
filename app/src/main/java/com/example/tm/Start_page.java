b4eda364d15b05d79727991cfe746572b96597fe
Bonnie18 <ndegwa.bonnie@gmail.com>
2019-06-03 11:54:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tm/Start_page.java
@@ -0,0 +1,13 @@
+package com.example.tm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+}
